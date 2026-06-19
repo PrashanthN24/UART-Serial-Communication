16fb40010ab78a9c865c0a7433256c0163db85e3
Prashanth N <nakkalaprashaanth@gmail.com>
2026-06-20 00:11:56 +0530

    Implemented baud rate generator

--- UART.srcs/sources_1/new/Baudrate_gen.v
+++ UART.srcs/sources_1/new/Baudrate_gen.v
@@ -21,6 +21,44 @@
 
 
 module Baudrate_gen(
+    input clk,
+    input rst,
+    output tx_enb,
+    output rx_enb
+);
 
-    );
+reg [12:0] tx_counter;
+reg [9:0] rx_counter;
+
+always @(posedge clk) begin
+    if (rst) begin
+        tx_counter <= 13'd0;
+    end else begin
+        if (tx_counter == 13'd5207) begin 
+            tx_counter <= 13'd0;
+        end else begin
+            tx_counter <= tx_counter + 1'b1; // Fixed to non-blocking
+        end
+    end
+end
+    
+// Receiver Counter Channel
+always @(posedge clk) begin
+    if (rst) begin
+        rx_counter <= 10'd0;
+    end else begin
+        if (rx_counter == 10'd324) 
+        begin 
+            rx_counter <= 10'd0;
+        end else begin
+            rx_counter <= rx_counter + 1'b1; 
+        end
+    end
+end
+    
+// Continuous assignments for your single-cycle enable pulses
+assign rx_enb = (rx_counter == 10'd0) ? 1'b1 : 1'b0;
+assign tx_enb = (tx_counter == 13'd0) ? 1'b1 : 1'b0;
+
 endmodule
+
